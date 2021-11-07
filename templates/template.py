f5d8f2460d03538a85b97951cba9df1163993eb8
nethish <nethish259@gmail.com>
2021-11-07 22:57:38 +0530

    Updated python fastio

--- templates/template.py
+++ templates/template.py
@@ -1,12 +1,23 @@
 import sys
 INF = 10**20
 MOD = 10**9 + 7
-I = lambda:list(map(int,input().split()))
 from math import gcd
 from math import ceil, log2, factorial
 from collections import defaultdict as dd, Counter
 from bisect import bisect_left as bl, bisect_right as br
- 
+
+def fastio():
+  import sys
+  from io import StringIO 
+  from atexit import register
+  global input
+  sys.stdin = StringIO(sys.stdin.read())
+  input = lambda : sys.stdin.readline().rstrip('\r\n')
+  sys.stdout = StringIO()
+  register(lambda : sys.__stdout__.write(sys.stdout.getvalue()))
+fastio()
+
+I = lambda:list(map(int,input().split()))
 TESTS = 1 
 """
 Facts and Data representation
